64d8795e8834c0f60520f27dca371163cf8e0ce6
AustinWelsh-Graham <Austin@austinatts.co.uk>
2025-02-18 22:59:25 +0000

    Designed the home page, updated colours and styles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.ticker)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
