e42acd65b84e1436acbfc061d0f4f6530f6842b6
AustinWelsh-Graham <Austin@austinatts.co.uk>
2025-02-20 00:05:52 +0000

    Added a listener to move to the settings page when button is pressed

--- app/src/main/java/uk/co/austinatts/toastynumberschallenge/MainActivity.kt
+++ app/src/main/java/uk/co/austinatts/toastynumberschallenge/MainActivity.kt
@@ -1,20 +1,27 @@
 package uk.co.austinatts.toastynumberschallenge
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import uk.co.austinatts.toastynumberschallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ibtnSettings.setOnClickListener {
+            navigateToActivity(SettingsActivity::class.java)
         }
+    }
+
+    private fun navigateToActivity(activityClass: Class<*>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
     }
 }
