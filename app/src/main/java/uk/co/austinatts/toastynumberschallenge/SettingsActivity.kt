76636770159b219116d034b00a0691289ab17cf8
AustinWelsh-Graham <Austin@austinatts.co.uk>
2025-02-20 00:11:30 +0000

    Added onCreate function to display layout

--- app/src/main/java/uk/co/austinatts/toastynumberschallenge/SettingsActivity.kt
+++ app/src/main/java/uk/co/austinatts/toastynumberschallenge/SettingsActivity.kt
@@ -1,6 +1,12 @@
 package uk.co.austinatts.toastynumberschallenge
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
 }
